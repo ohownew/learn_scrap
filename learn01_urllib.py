abc3b2c0fef24086173d4dbe918194308459d7de
Yuan Zhong <2395972356@qq.com>
2023-06-14 00:27:52 +0800

    Add some lines (#2)
    Co-authored-by: zhongy <zhongy1024@qq.com>

--- learn01_urllib.py
+++ learn01_urllib.py
@@ -23,3 +23,7 @@ import urllib.parse
 data = bytes(urllib.parse.urlencode({'name': 'germey'}), encoding='utf-8')
 response = urllib.request.urlopen('https://httpbin.org/post', data=data)
 print(response.read().decode('utf-8'))
+
+
+response = urllib.request.urlopen('https://httpbin.org/get', timeout=0.1)
+print(response.read())
